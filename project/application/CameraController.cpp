46f7449ed9c52ac7fd2fd7134e0584e85afcc817
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2025-12-24 16:29:55 +0900

    あ

--- project/application/CameraController.cpp
+++ project/application/CameraController.cpp
@@ -1,16 +1,14 @@
 #include "CameraController.h"
 #include "Camera.h"
-#include <imgui.h>
 #include "GameBase.h"
-CameraController::~CameraController() {
-	
-}
+#include <imgui.h>
+CameraController::~CameraController() {}
 void CameraController::Initialize() {
 
 	transform_ = {
-		.scale{1.0f, 1.0f, 1.0f},
-		.rotate{0.15f, 0.0f, 0.0f},
-		.translate{0.0f, 10.0f, -50.0f}
+	    .scale{1.0f,        1.0f,      1.0f  },
+        .rotate{orbitPitch_, orbitYaw_, 0.0f  },
+        .translate{0.0f,        10.0f,     -50.0f}
     };
 
 	camera_ = std::make_unique<Camera>();
@@ -18,57 +16,47 @@ void CameraController::Initialize() {
 }
 void CameraController::Update() {
 
-	#ifdef USE_IMGUI
+#ifdef USE_IMGUI
 
 	if (ImGui::Begin("CameraController")) {
 		ImGui::DragFloat3("CameraScale", &transform_.scale.x, 0.01f);
-		ImGui::DragFloat3("CameraRotate", &transform_.rotate.x, 0.01f);
+		ImGui::DragFloat("OrbitYaw", &orbitYaw_, 0.01f);
+		ImGui::DragFloat("OrbitPitch", &orbitPitch_, 0.01f);
 		ImGui::DragFloat3("CameraTranslate", &transform_.translate.x, 0.1f);
 	}
 	ImGui::End();
-	
-	if (GameBase::GetInstance()->PushKey(DIK_LEFT)) {
-		transform_.rotate.y -= cameraSpeed_;
-	}
-	if (GameBase::GetInstance()->PushKey(DIK_RIGHT)) {
-		transform_.rotate.y += cameraSpeed_;
-	}
-	if (GameBase::GetInstance()->PushKey(DIK_UP)) {
-		transform_.rotate.x -= cameraSpeed_;
-	}
-	if (GameBase::GetInstance()->PushKey(DIK_DOWN)) {
-		transform_.rotate.x += cameraSpeed_;
-	}
+
 #endif
-	const float distance = 25.0f; 
-	const float height = 5.0f;   
-
-	Vector3 backDir = {sinf(playerYaw), 0.0f, cosf(playerYaw)};
-
-	transform_.translate = playerPos - backDir * distance + Vector3{0.0f, height, 0.0f};
-
 	if (GameBase::GetInstance()->PushKey(DIK_LEFT)) {
-		transform_.rotate.y += cameraSpeed_;
+		orbitYaw_ -= cameraSpeed_;
 	}
 	if (GameBase::GetInstance()->PushKey(DIK_RIGHT)) {
-		transform_.rotate.y -= cameraSpeed_;
+		orbitYaw_ += cameraSpeed_;
 	}
 	if (GameBase::GetInstance()->PushKey(DIK_UP)) {
-		transform_.rotate.x -= cameraSpeed_;
+		orbitPitch_ += cameraSpeed_;
 	}
 	if (GameBase::GetInstance()->PushKey(DIK_DOWN)) {
-		transform_.rotate.x += cameraSpeed_;
+		orbitPitch_ -= cameraSpeed_;
 	}
+
+	const float maxPitch = 1.2f;
+	const float minPitch = -1.2f;
+	if (orbitPitch_ > maxPitch) {
+		orbitPitch_ = maxPitch;
+	}
+	if (orbitPitch_ < minPitch) {
+		orbitPitch_ = minPitch;
+	}
+
+	const float distance = 25.0f;
+	Vector3 orbitDir = {sinf(orbitYaw_) * cosf(orbitPitch_), -sinf(orbitPitch_), cosf(orbitYaw_) * cosf(orbitPitch_)};
+
+	transform_.translate = playerPos - orbitDir * distance;
+	transform_.rotate = {orbitPitch_, orbitYaw_, 0.0f};
 
 	camera_->SetTransform(transform_);
 	camera_->Update();
-
 }
-void CameraController::SpecialAttackUpdate() {
-	
-
-	
-
-}
-	Camera* CameraController::GetCamera() {
-	return camera_.get();}
+void CameraController::SpecialAttackUpdate() {}
+Camera* CameraController::GetCamera() { return camera_.get(); }
--- project/application/CameraController.h
+++ project/application/CameraController.h
@@ -8,22 +8,21 @@ class CameraController {
 	std::unique_ptr<Camera> camera_;
 	float cameraSpeed_ = 0.1f;
 	Vector3 playerPos = {0.0f, 0.0f, 0.0f};
-	float playerYaw = 0.0f;
+	float orbitYaw_ = 0.0f;
+	float orbitPitch_ = 0.15f;
 
-	public:
+public:
 	~CameraController();
 	void Initialize();
 	void Update();
 	void SpecialAttackUpdate();
-	
+
 	Camera* GetCamera();
-	Transform GetTransform() { return transform_;}
+	Transform GetTransform() { return transform_; }
 	void SetTransform(Transform transform) { transform_ = transform; }
 	void SetPlayerPos(const Vector3& pos) { playerPos = pos; }
-	void SetPlayerYaw(float yaw) { playerYaw = yaw; }
-	void SetTranslate(const Vector3& translate) { 
+	void SetTranslate(const Vector3& translate) {
 		transform_.translate.x = translate.x;
 		transform_.translate.z = translate.z;
 	}
 };
-
