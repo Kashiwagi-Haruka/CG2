15428fc0d19e21efb1826bb18d5512f7c84a61eb
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-01-06 19:36:08 +0900

    a

--- project/application/SceneTransition.cpp
+++ project/application/SceneTransition.cpp
@@ -16,10 +16,10 @@ void SceneTransition::Initialize(bool isIn){
 	isIn_ = isIn;
 	if (isIn_) {
 		fadeSPData.size = {0, 720};
-		fadeSPData.translate = {-1500, 0}; 
+		fadeSPData.translate = {1280, 0}; 
 	} else {
-		fadeSPData.size = {1500, 720};
-		fadeSPData.translate = {0, 0}; 
+		fadeSPData.size = {1800, 720};
+		fadeSPData.translate = {-520, 0}; 
 	}
 	
 	fadeSPData.sprite->SetScale(fadeSPData.size);
@@ -32,15 +32,15 @@ void SceneTransition::Update() {
 
 	
 	if (isIn_) {
-		fadeSPData.size.x += 10.0f;
-		fadeSPData.translate.x -= 10.0f;
-		if (fadeSPData.size.x >= 1500.0f) {
-			fadeSPData.size.x = 1500.0f;
+		fadeSPData.size.x += 40.0f;
+		fadeSPData.translate.x -= 40.0f;
+		if (fadeSPData.size.x >= 1800.0f) {
+			fadeSPData.size.x = 1800.0f;
 			isEnd = true;
 		}
 	} else {
-		fadeSPData.size.x -= 10.0f;
-		fadeSPData.translate.x += 10.0f;
+		fadeSPData.size.x -= 40.0f;
+		fadeSPData.translate.x += 40.0f;
 		if (fadeSPData.size.x <=  0.0f) {
 			fadeSPData.size.x = 0.0f;
 			isEnd = true;
