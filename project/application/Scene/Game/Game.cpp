191b11fb71ecc56c279fa57f0d7c997d1c22802c
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-01-20 12:59:27 +0900

    a

--- project/application/Scene/Game/Game.cpp
+++ project/application/Scene/Game/Game.cpp
@@ -1,30 +1,23 @@
 #include "Game.h"
 #include "SceneManager.h"
 
-
-void Game::Initialize(){
+void Game::Initialize() {
 	FrameWork::Initialize();
 	GameBase::GetInstance()->Initialize(L"LE2A_04_カシワギハルカ_氷華", 1280, 720);
 
-
-	
-
 	SetUnhandledExceptionFilter(GameBase::GetInstance()->ExportDump);
-	
 
 	sceneFactory_ = std::make_unique<SceneFactory>();
 	SceneManager::GetInstance()->SetSceneFactory(sceneFactory_.get());
 	/*SceneManager::GetInstance()->ChangeScene("Title");*/
-	/*SceneManager::GetInstance()->ChangeScene("Sample");*/
-	SceneManager::GetInstance()->ChangeScene("Game");
+	SceneManager::GetInstance()->ChangeScene("Sample");
+	/*SceneManager::GetInstance()->ChangeScene("Game");*/
 	/*SceneManager::GetInstance()->ChangeScene("Result");*/
 }
 
-void Game::Update(){
+void Game::Update() {
 	FrameWork::Update();
 	GameBase::GetInstance()->BeginFlame();
-
-
 
 	SceneManager::GetInstance()->Update();
 	if (!GameBase::GetInstance()->ProcessMessage()) {
@@ -34,18 +27,16 @@ void Game::Update(){
 
 void Game::Draw() {
 	SceneManager::GetInstance()->Draw();
-	
+
 	// ゲームの処理
 	GameBase::GetInstance()->EndFlame();
-
 }
 
 void Game::Finalize() {
-	
+
 	SceneManager::GetInstance()->Finalize();
 	GameBase::GetInstance()->Finalize();
 	d3dResourceLeakChecker.LeakChecker();
 	CoUninitialize();
 	FrameWork::Finalize();
-
 }
--- project/application/Scene/Game/Game.h
+++ project/application/Scene/Game/Game.h
@@ -1,29 +1,20 @@
 #pragma once
-#include "FrameWork.h"
-#include"GameBase.h"
 #include "D3DResourceLeakChecker.h"
-#include <memory>
+#include "FrameWork.h"
+#include "GameBase.h"
 #include "Scene/SceneFactory/SceneFactory.h"
+#include <memory>
 class BaseScene;
-class Game :public FrameWork{
-	
-
+class Game : public FrameWork {
 
 	D3DResourceLeakChecker d3dResourceLeakChecker;
 
 	std::unique_ptr<SceneFactory> sceneFactory_ = nullptr;
 
-
-	public:
-
+public:
 	void Initialize() override;
 	void Update() override;
 	void Draw() override;
 	void Finalize() override;
-	~Game()=default;
-	
-
-
-
-
+	~Game() = default;
 };
