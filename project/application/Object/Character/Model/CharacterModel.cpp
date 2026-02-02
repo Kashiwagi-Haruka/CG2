52c3b181d47b3f1134d3847c77cbefc8fd5611f8
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-02-03 01:14:54 +0900

    a

--- project/application/Object/Character/Model/CharacterModel.cpp
+++ project/application/Object/Character/Model/CharacterModel.cpp
@@ -3,4 +3,5 @@
 void CharacterModel::LoadModel() { 
 	ModelManager::GetInstance()->LoadGltfModel("Resources/3d", "sizuku"); 
 	ModelManager::GetInstance()->LoadModel("Resources/3d", "Enemy");
+	ModelManager::GetInstance()->LoadGltfModel("Resources/3d", "WaterBoss");
 }
