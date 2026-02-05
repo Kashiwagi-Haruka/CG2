4fd567728e2cfd7349e580b82f0251ed09d624ae
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-02-05 14:00:17 +0900

    Reapply "a"
    This reverts commit a8abfaa054d097750c6542f806f2a344b475b43e.

--- project/application/Object/Player/PlayerSpecialAttack.h
+++ project/application/Object/Player/PlayerSpecialAttack.h
@@ -5,19 +5,9 @@
 #include <vector>
 class PlayerSpecialAttack {
 
-	std::unique_ptr<std::vector<Object3d>> iceFlowers_;
-	std::vector<Transform> iceFlowerTransforms_;
-	std::unique_ptr<Object3d> debugBox_;
+
 	Camera* camera_ = nullptr;
 	Transform transform_;
-	float radius = 3.0f;
-	float fallSpeed_ = 0.25f;
-	float startHeight_ = 6.0f;
-	bool isSpecialEnd_;
-	int specialTime_ = 0;
-	int specialTimeMax_ = 60;
-
-	void EnsureIceFlowerCount(int count);
 
 public:
 	PlayerSpecialAttack();
@@ -25,8 +15,5 @@ public:
 	void Update(const Transform& playerTransform);
 	void Draw();
 	void SetCamera(Camera* camera) { camera_ = camera; }
-	void StartAttack(const Transform& playerTransform, int iceCount);
-	bool IsSpecialEnd() { return isSpecialEnd_; }
-	bool IsDamaging() const { return !isSpecialEnd_; }
-	const std::vector<Transform>& GetIceFlowerTransforms() const { return iceFlowerTransforms_; }
+
 };
