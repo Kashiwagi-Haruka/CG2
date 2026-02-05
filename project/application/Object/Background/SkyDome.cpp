d2ecb78a6f2d521de5e47eebac79647c0f273d61
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-02-05 13:33:48 +0900

    a

--- project/application/Object/Background/SkyDome.cpp
+++ project/application/Object/Background/SkyDome.cpp
@@ -15,7 +15,7 @@ void SkyDome::Initialize(Camera* camera) {
 	skyDomeObject_->SetModel("skyDome");
 	camera_ = camera;
 	skyDomeObject_->SetCamera(camera_);
-	transform_.scale = {50.0f, 50.0f, 50.0f};
+	transform_.scale = {100.0f, 100.0f, 100.0f};
 	transform_.rotate = {0.0f, 0.0f, 0.0f};
 	transform_.translate = {-75.0f, 2.50f, -75.0f};
 	skyDomeObject_->SetTransform(transform_);
