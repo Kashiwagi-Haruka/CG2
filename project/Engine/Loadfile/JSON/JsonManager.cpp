5364e6ec604eef3b379542f85ca31a5a8ce5ab7b
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-02-20 15:27:19 +0900

    a

--- project/Engine/Loadfile/JSON/JsonManager.cpp
+++ project/Engine/Loadfile/JSON/JsonManager.cpp
@@ -1,6 +1,15 @@
 #include "JsonManager.h"
 #include "Logger.h"
+#include <filesystem>
 #include <fstream>
+
+namespace {
+std::filesystem::path ResolveJsonPath(const std::string& filename) {
+	const std::filesystem::path inputPath(filename);
+	const std::filesystem::path fileNameOnly = inputPath.filename();
+	return std::filesystem::path("Resources") / "JSON" / fileNameOnly;
+}
+} // namespace
 
 JsonManager* JsonManager::GetInstance() {
 	static JsonManager instance;
@@ -10,9 +19,10 @@ JsonManager* JsonManager::GetInstance() {
 bool JsonManager::LoadJson(const std::string& filename) {
 	data_.clear();
 
-	std::ifstream file(filename);
+	const std::filesystem::path jsonPath = ResolveJsonPath(filename);
+	std::ifstream file(jsonPath);
 	if (!file.is_open()) {
-		Logger::Log("JSON ファイルを開けません\n");
+		Logger::Log(std::string("JSON ファイルを開けません: ") + jsonPath.string() + "\n");
 		return false;
 	}
 
@@ -27,9 +37,12 @@ bool JsonManager::LoadJson(const std::string& filename) {
 	return true;
 }
 bool JsonManager::SaveJson(const std::string& filename) const {
-	std::ofstream file(filename);
+	const std::filesystem::path jsonPath = ResolveJsonPath(filename);
+	std::filesystem::create_directories(jsonPath.parent_path());
+
+	std::ofstream file(jsonPath);
 	if (!file.is_open()) {
-		Logger::Log("JSON ファイルを書き込めません\n");
+		Logger::Log(std::string("JSON ファイルを書き込めません: ") + jsonPath.string() + "\n");
 		return false;
 	}
 
