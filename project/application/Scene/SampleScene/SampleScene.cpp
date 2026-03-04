3b642861e47131eb589811021018bac2ce8e5371
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-03-04 14:07:12 +0900

    a

--- project/application/Scene/SampleScene/SampleScene.cpp
+++ project/application/Scene/SampleScene/SampleScene.cpp
@@ -85,6 +85,7 @@ void SampleScene::Initialize() {
         .rotate{std::numbers::pi_v<float> * 3.0f / 2.0f, 0.0f, 0.0f},
         .translate{3.0f,                                    3.5f, 2.0f}
     };
+	portalTextureCameraATransform_ = portalBTransform_;
 	portalTextureCameraATransform_.scale = {1.0f, 1.0f, 1.0f};
 	portalTextureCameraA_->SetTransform(MakeOppositeSidePortalCameraTransform(portalTextureCameraATransform_));
 	portalTextureCameraA_->Update();
@@ -438,10 +439,10 @@ void SampleScene::Update() {
 	//portalTextureCameraATransform_ = portalATransform_;
 	//portalTextureCameraBTransform_ = portalBTransform_;
 	portalTextureCameraATransform_.scale = {1.0f, 1.0f, 1.0f};
-	portalTextureCameraA_->SetTransform(portalTextureCameraATransform_);
+	portalTextureCameraA_->SetTransform(MakeOppositeSidePortalCameraTransform(portalTextureCameraATransform_));
 	portalTextureCameraA_->Update();
 	portalTextureCameraBTransform_.scale = {1.0f, 1.0f, 1.0f};
-	portalTextureCameraB_->SetTransform(portalTextureCameraBTransform_);
+	portalTextureCameraB_->SetTransform(MakeOppositeSidePortalCameraTransform(portalTextureCameraBTransform_));
 	portalTextureCameraB_->Update();
 	portalMeshA_->SetTransform(portalATransform_);
 	portalMeshA_->SetTextureCamera(portalTextureCameraA_.get());
