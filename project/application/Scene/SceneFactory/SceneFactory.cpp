d536e1ccea96ab8053e66dbc97a854f967f0ded3
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-03-05 16:07:46 +0900

    a

--- project/application/Scene/SceneFactory/SceneFactory.cpp
+++ project/application/Scene/SceneFactory/SceneFactory.cpp
@@ -3,8 +3,9 @@
 #include "Scene/GameScene.h"
 #include "Scene/ResultScene.h"
 #include "Scene/SampleScene/SampleScene.h"
+#include "Scene/TD3_4month/ProtoPush.h"
+#include "Scene/TD3_4month/ShadowGameScene.h"
 #include "Scene/TitleScene.h"
-#include"Scene/TD3_4month/ShadowGameScene.h"
 #include <memory>
 std::unique_ptr<BaseScene> SceneFactory::CreateScene(const std::string& sceneName) {
 	std::unique_ptr<BaseScene> scene = nullptr;
@@ -20,14 +21,16 @@ std::unique_ptr<BaseScene> SceneFactory::CreateScene(const std::string& sceneNam
 	} else if (sceneName == "Sample") {
 		scene = std::make_unique<SampleScene>();
 	} else if (sceneName == "ShadowGame") {
-		//TD3用のシーンの追加
+		// TD3用のシーンの追加
 		scene = std::make_unique<ShadowGameScene>();
+	} else if (sceneName == "ProtoPush") {
+		scene = std::make_unique<ProtoPush>();
 	}
 
 	return scene;
 }
 std::vector<std::string> SceneFactory::GetSceneNames() const {
 	return {
-	    "Title", "Game", "Result", "GameOver", "Sample", "ShadowGame",
+	    "Title", "Game", "Result", "GameOver", "Sample", "ShadowGame", "ProtoPush",
 	};
 }
