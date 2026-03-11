41cba7b02c49edf6422fce68ab5564c92dd81ab9
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-03-11 11:07:57 +0900

    a

--- project/application/GameSave/GameSave.h
+++ project/application/GameSave/GameSave.h
@@ -1,7 +1,20 @@
 #pragma once
+#include "Transform.h"
+#include "Engine/Loadfile/JSON/JsonManager.h"
 class GameSave {
 
+	struct PlayerSaveData{
+		Transform transform;
+	};
+	struct CameraSaveData{
+		Transform transform;
+	};
 
+	PlayerSaveData playerSaveData_;
+	CameraSaveData cameraSaveData_;
+
+
+public:
 
 	void Save();
 	void Load();
