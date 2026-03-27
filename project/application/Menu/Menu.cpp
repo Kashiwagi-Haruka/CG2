270325103fb472e707c59bd7cb3bac86e34bf5d1
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-03-27 15:20:36 +0900

    a

--- project/application/Menu/Menu.cpp
+++ project/application/Menu/Menu.cpp
@@ -1,86 +1,122 @@
 #include "Menu.h"
-#include "GameObject/KeyBindConfig.h"
-#include "application/Color/Color.h"
 #include "GameBase.h"
-#include "WinApp.h"
+#include "GameObject/KeyBindConfig.h"
 #include "Sprite/SpriteCommon.h"
-void Menu::ChangeMenu(std::string MenuName) {
-	currentMenuName_ = MenuName; 
+#include "WinApp.h"
+#include "application/Color/Color.h"
+
+namespace {
+std::u32string ToMenuLabel(const std::string& menuName) {
+	if (menuName == "Game") {
+		return U"ゲームに戻る";
+	}
+	if (menuName == "Save") {
+		return U"セーブ";
+	}
+	if (menuName == "Option") {
+		return U"オプション";
+	}
+	if (menuName == "Title") {
+		return U"タイトルへ戻る";
+	}
+	if (menuName == "GameEnd") {
+		return U"ゲームを終了";
+	}
+	return U"";
 }
+} // namespace
+
+void Menu::ChangeMenu(std::string MenuName) { currentMenuName_ = MenuName; }
+
 void Menu::Initialize() {
 	// フォントハンドル
 	fontHandle_ = FreeTypeManager::CreateFace("Resources/TD3_3102/Irohakaku/irohakakuC-Medium.ttf", 0);
-	FreeTypeManager::SetPixelSizes(fontHandle_, 64, 64);
-	currentMenuName_ = "Option"; 
+	FreeTypeManager::SetPixelSizes(fontHandle_, 48, 48);
+	currentMenuName_ = "Option";
 	option_ = std::make_unique<Option>();
 	option_->Initialize();
 
 	menuText_.Initialize(fontHandle_);
-	menuText_.SetString(U"p-再打刻");
-	menuText_.SetPosition({WinApp::kClientWidth/2.0f,WinApp::kClientHeight/2.0f});
+	menuText_.SetString(U"メニュー");
+	menuText_.SetPosition({WinApp::kClientWidth / 2.0f, WinApp::kClientHeight / 2.0f - 180.0f});
 	menuText_.SetColor(COLOR::WHITE);
 	menuText_.SetAlign(TextAlign::Center);
+	menuText_.UpdateLayout(false);
 
+	GameText_.Initialize(fontHandle_);
+	GameText_.SetString(ToMenuLabel("Game"));
+	GameText_.SetPosition({WinApp::kClientWidth / 2.0f, WinApp::kClientHeight / 2.0f - 60.0f});
+	GameText_.SetAlign(TextAlign::Center);
+	GameText_.UpdateLayout(false);
 
+	SaveText_.Initialize(fontHandle_);
+	SaveText_.SetString(ToMenuLabel("Save"));
+	SaveText_.SetPosition({WinApp::kClientWidth / 2.0f, WinApp::kClientHeight / 2.0f});
+	SaveText_.SetAlign(TextAlign::Center);
+	SaveText_.UpdateLayout(false);
+
+	OptionText_.Initialize(fontHandle_);
+	OptionText_.SetString(ToMenuLabel("Option"));
+	OptionText_.SetPosition({WinApp::kClientWidth / 2.0f, WinApp::kClientHeight / 2.0f + 60.0f});
+	OptionText_.SetAlign(TextAlign::Center);
+	OptionText_.UpdateLayout(false);
+
+	TitleText_.Initialize(fontHandle_);
+	TitleText_.SetString(ToMenuLabel("Title"));
+	TitleText_.SetPosition({WinApp::kClientWidth / 2.0f, WinApp::kClientHeight / 2.0f + 120.0f});
+	TitleText_.SetAlign(TextAlign::Center);
+	TitleText_.UpdateLayout(false);
+
+	GameEndText_.Initialize(fontHandle_);
+	GameEndText_.SetString(ToMenuLabel("GameEnd"));
+	GameEndText_.SetPosition({WinApp::kClientWidth / 2.0f, WinApp::kClientHeight / 2.0f + 180.0f});
+	GameEndText_.SetAlign(TextAlign::Center);
+	GameEndText_.UpdateLayout(false);
 }
 
 void Menu::Update() {
-	menuText_.Update();
+	auto setDefaultColor = [](Text& text) {
+		text.SetColor(COLOR::WHITE);
+		text.UpdateLayout(false);
+	};
+	setDefaultColor(GameText_);
+	setDefaultColor(SaveText_);
+	setDefaultColor(OptionText_);
+	setDefaultColor(TitleText_);
+	setDefaultColor(GameEndText_);
+
 	if (currentMenuName_ == "Game") {
-		// ゲームに戻る
+		GameText_.SetColor(COLOR::RED);
+		GameText_.UpdateLayout(false);
 	} else if (currentMenuName_ == "Save") {
-		// セーブ
+		SaveText_.SetColor(COLOR::RED);
+		SaveText_.UpdateLayout(false);
 	} else if (currentMenuName_ == "Option") {
-		// オプション
+		OptionText_.SetColor(COLOR::RED);
+		OptionText_.UpdateLayout(false);
 	} else if (currentMenuName_ == "Title") {
-		// タイトル
+		TitleText_.SetColor(COLOR::RED);
+		TitleText_.UpdateLayout(false);
 	} else if (currentMenuName_ == "GameEnd") {
-		// ゲームを終わる
+		GameEndText_.SetColor(COLOR::RED);
+		GameEndText_.UpdateLayout(false);
 	}
-	if (isTrigger_) {
-		if (currentMenuName_ == "Game") {
-			// ゲームに戻る
-		} else if (currentMenuName_ == "Save") {
-			// セーブ
-		} else if (currentMenuName_ == "Option") {
-			// オプション
-			option_->Update();
-		} else if (currentMenuName_ == "Title") {
-			// タイトル
-		} else if (currentMenuName_ == "GameEnd") {
-			// ゲームを終わる
-		}
+
+	if (isTrigger_ && currentMenuName_ == "Option") {
+		option_->Update();
 	}
 }
 
 void Menu::Draw() {
 	SpriteCommon::GetInstance()->DrawCommonFont();
 	menuText_.Draw();
-	if (currentMenuName_ == "Game") {
-		// ゲームに戻る
-	} else if (currentMenuName_ == "Save") {
-		// セーブ
-	} else if (currentMenuName_ == "Option") {
-		// オプション
-	} else if (currentMenuName_ == "Title") {
-		// タイトル
-	} else if (currentMenuName_ == "GameEnd") {
-		// ゲームを終わる
-	}
-	if (isTrigger_) {
-		if (currentMenuName_ == "Game") {
-			// ゲームに戻る
-		} else if (currentMenuName_ == "Save") {
-			// セーブ
-		} else if (currentMenuName_ == "Option") {
-			// オプション
-			option_->Draw();
-		} else if (currentMenuName_ == "Title") {
-			// タイトル
-		} else if (currentMenuName_ == "GameEnd") {
-			// ゲームを終わる
-		}
-	
-	}
+	GameText_.Draw();
+	SaveText_.Draw();
+	OptionText_.Draw();
+	TitleText_.Draw();
+	GameEndText_.Draw();
 
+	if (isTrigger_ && currentMenuName_ == "Option") {
+		option_->Draw();
+	}
 }
