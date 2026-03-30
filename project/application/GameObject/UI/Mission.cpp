b6034568042ca678ad24a42673dfb5b26ccd73c3
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-03-30 17:22:47 +0900

    a

--- project/application/GameObject/UI/Mission.cpp
+++ project/application/GameObject/UI/Mission.cpp
@@ -1,16 +1,22 @@
 #include "Mission.h"
 #include "Engine/Texture/Data/Color.h"
 #include "Sprite/SpriteCommon.h"
+#include "Color/Color.h"
+Mission::Mission() = default;
 void Mission::Initialize() {	
 	fontHandle_ = FreeTypeManager::CreateFace("Resources/TD3_3102/Irohakaku/irohakakuC-Medium.ttf", 0);
+	FreeTypeManager::SetPixelSizes(fontHandle_, 24, 24);
 	MissionText_.Initialize(fontHandle_);
 	MissionText_.SetString(U"ミッション");
-	MissionText_.SetPosition({20, 300});
+	MissionText_.SetPosition({50, 200});
 	MissionText_.SetColor(Color::RGBAToVector4(180,180,50,255));
 	MissionText_.SetAlign(TextAlign::Left);
 	MissionText_.UpdateLayout(false);
 }
-void Mission::Update() {}
+void Mission::Update() {
+	/*MissionText_.Update();*/
+	MissionText_.UpdateLayout(false);
+}
 void Mission::Draw() {
 	SpriteCommon::GetInstance()->DrawCommonFont();
 	MissionText_.Draw(); }
