d7a9b145675e4358cea12da41d038b862c855b4d
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-04-03 17:16:28 +0900

    a

--- project/Engine/ScreenShot/ScreenShot.h
+++ project/Engine/ScreenShot/ScreenShot.h
@@ -7,6 +7,6 @@ class DirectXCommon;
 class ScreenShot {
 public:
 	// バックバッファを画像として保存する
-	// filePath は UTF-8 文字列で指定可能
+	// filePath は UTF-8 文字列で指定可能（PNGとして保存）
 	static bool SaveBackBuffer(DirectXCommon* dxCommon, const std::string& filePath);
 };
