f97437a44358f35345c6c790d9ea2ecaea0e194d
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-04-03 10:07:45 +0900

    a

--- project/application/Scene/SampleScene/SampleScene.cpp
+++ project/application/Scene/SampleScene/SampleScene.cpp
@@ -88,7 +88,7 @@ void SampleScene::Initialize() {
 	uvBallObj_->SetCamera(camera_.get());
 	uvBallObj_->SetModel("uvBall");
 	uvBallObj_->SetOutlineWidth(uvBallOutlineWidth_);
-	uvBallObj_->SetOutlineColor({1,1,1,1});
+	uvBallObj_->SetOutlineColor(uvBallOutlineColor_);
 	
 	fieldObj_->Initialize();
 	fieldObj_->SetCamera(camera_.get());
