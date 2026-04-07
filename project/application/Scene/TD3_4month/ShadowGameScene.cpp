529f822006333e9d1c38b1cc5f3a2529a6f269d1
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-04-08 00:10:03 +0900

    poi

--- project/application/Scene/TD3_4month/ShadowGameScene.cpp
+++ project/application/Scene/TD3_4month/ShadowGameScene.cpp
@@ -30,48 +30,7 @@ ShadowGameScene::ShadowGameScene()
     cameraController_ = CameraController::GetInstance();
     cameraController_->SetPlayer(player_.get());
 
-    //テスト地面
-    testField_ = std::make_unique<TestField>();
-    // ホワイトボード管理
-    whiteBoardManager_ = std::make_unique<WhiteBoardManager>(&player_->GetTransform().translate);
-    // ポータル管理
-    portalManager_ = std::make_unique<PortalManager>(&player_->GetTransform().translate, whiteBoardManager_.get());
-
-    //PC
-    pc_ = std::make_unique<PC>();
-    // コーヒー缶
-    coffees_ = std::make_unique<Coffees>();
-    //携帯打刻機
-    timeCardWatch_ = std::make_unique<TimeCardWatch>();
-    timeCardWatch_->SetPlayer(player_.get());
-
-    //懐中電灯
-    flashlight_ = std::make_unique<Flashlight>();
-    flashlight_->SetPlayer(player_.get());
-    // 鍵管理
-    key_ = std::make_unique<Key>();
-    // 枝豆管理
-    edamame_ = std::make_unique<Edamame>();
-    //ドア
-    door_ = std::make_unique<Door>();
-    //ロッカー
-    lockerManager_ = std::make_unique<LockerManager>();
-    //壁管理
-    wallManager_ = std::make_unique<WallManager>();
-    //壁管理
-    wallManager2_ = std::make_unique<WallManager2>();
-    //自販機
-    vendingMac_ = std::make_unique<VendingMac>();
-    //椅子
-    chairManager_ = std::make_unique<ChairManager>();
-    //机
-    deskManager_ = std::make_unique<DeskManager>();
-    //打刻機
-    timeCard_ = std::make_unique<TimeCard>();
-    //タイムカードラック
-    timeCardRack_ = std::make_unique<TimeCardRack>();
-    //箱管理
-    boxManager_ = std::make_unique<BoxManager>();
+   
     // エレベーター
     elevator_ = std::make_unique<Elevator>();
     //セーブポイント紳士
@@ -79,12 +38,8 @@ ShadowGameScene::ShadowGameScene()
     //エレベータールーム
     elevatorRoomManager_ = std::make_unique<ElevatorRoomManager>();
 
-    //衝突管理
-    collisionManager_ = std::make_unique<CollisionManager>();
-    //UI管理
-    uiManager_ = std::make_unique<UIManager>();
-    //最初のイベント
-    firstEvent_ = std::make_unique<FirstGameEvent>();
+	// UI管理
+	uiManager_ = std::make_unique<UIManager>();
     //PlayerCameraをセットする
     SetPlayerCamera(cameraController_->GetPlayerCamera());
 
@@ -129,45 +84,7 @@ void ShadowGameScene::Initialize()
 
     InitializeLights();
 
-    //テスト地面
-    testField_->Initialize();
-    //ホワイトボード管理
-    whiteBoardManager_->Initialize();
-    // ポータル管理
-    portalManager_->Initialize();
-    //PC
-    pc_->Initialize();
-    // コーヒー缶
-    coffees_->Initialize();
-    coffees_->SetFloorY(0.0f);
-    coffees_->SetRoomBounds(-40.0f, 40.0f, -40.0f, 40.0f);
-    coffees_->SetSpawnContainment({ 0.0f, 0.0f, 0.0f }, 0.0f, 0.0f);
-    //携帯打刻機
-    timeCardWatch_->Initialize();
-    // 鍵
-    key_->Initialize();
-    // 枝豆
-    edamame_->Initialize();
-    //椅子
-    chairManager_->Initialize();
-    //壁
-    wallManager_->Initialize();
-    //壁
-    wallManager2_->Initialize();
-    //自販機
-    vendingMac_->Initialize();
-    //ドア
-    door_->Initialize();
-    //ロッカー
-    lockerManager_->Initialize();
-    //デスク管理
-    deskManager_->Initialize();
-    //打刻機
-    timeCard_->Initialize();
-    //タイムカードラック
-    timeCardRack_->Initialize();
-    //箱管理
-    boxManager_->Initialize();
+
     // エレベーター
     elevator_->Initialize();
     //セーブポイント紳士
@@ -179,19 +96,18 @@ void ShadowGameScene::Initialize()
     //カメラをセットする
     SetSceneCameraForDraw(cameraController_->GetPlayerCamera()->GetCamera());
 
-    //最初のイベントをセットする
-    currentEvent_ = firstEvent_.get();
+
 
 	stageManager_->ChangeStage("MirrorStage");
     Update();
 
-    currentEvent_->StartEvent();
+
 }
 
 void ShadowGameScene::Update()
 {
 	stageManager_->Update();
-    currentEvent_->Update();
+ 
     //カメラの更新処理
     UpdateCamera();
     //ライトの更新処理
@@ -249,69 +165,13 @@ void ShadowGameScene::DebugImGui()
 
 void ShadowGameScene::CheckCollision()
 {
-    //ホワイトボードとrayの当たり判定作成する
-    portalManager_->CheckCollision();
-    door_->CheckCollision();
-    //コーヒー排出する
-    if (vendingMac_->ConsumeInteractRequest()) {
-        coffees_->StartSpill();
-    }
-
-    collisionManager_->ClearColliders();
-    collisionManager_->AddCollider(player_.get());
-
-    for (auto& portal : portalManager_->GetPortals()) {
-        if (!portal->GetIsPlayerHit()) {
-            //プレイヤーがヒットしてないときコライダーリストに追加する
-            collisionManager_->AddCollider(portal.get());
-        } else {
-            break;
-        }
-    }
-
-    for (auto& whiteBoard : whiteBoardManager_->GetWhiteBoards()) {
-        collisionManager_->AddCollider(whiteBoard.get());
-    }
-    for (auto& wall : wallManager_->GetWalls()) {
-        collisionManager_->AddCollider(wall.get());
-    }
-
-    for (auto& wall : wallManager2_->GetWalls()) {
-        collisionManager_->AddCollider(wall.get());
-    }
-
+	collisionManager_->AddCollider(player_.get());
     for (auto& wall : elevatorRoomManager_->GetWalls()) {
         collisionManager_->AddCollider(wall.get());
     }
-    
-    for (auto& chair : chairManager_->GetChairs()) {
-        collisionManager_->AddCollider(chair.get());
-    }
-    for (auto& locker : lockerManager_->GetLockers()) {
-        collisionManager_->AddCollider(locker.get());
-    }
-    for (auto& desk : deskManager_->GetDesks()) {
-        collisionManager_->AddCollider(desk.get());
-    }
-    for (auto& box : boxManager_->GetBoxes()) {
-        collisionManager_->AddCollider(box.get());
-    }
-
-    for (auto& system : elevator_->GetAutoLockSys()) {
-        collisionManager_->AddCollider(system.get());
-    }
-
-    collisionManager_->AddCollider(vendingMac_.get());
-    collisionManager_->AddCollider(flashlight_.get());
-    collisionManager_->AddCollider(testField_.get());
-    collisionManager_->AddCollider(door_->GetAutoLockSystem().get());
-    if (!door_->GetIsOpen()) {
-        collisionManager_->AddCollider(door_.get());
-    }
-
-    collisionManager_->AddCollider(key_.get());
-    collisionManager_->AddCollider(edamame_->GetEdamameModel().get());
-    collisionManager_->AddCollider(pc_.get());
+	for (auto& system : elevator_->GetAutoLockSys()) {
+		collisionManager_->AddCollider(system.get());
+	}
 
     collisionManager_->CheckAllCollisions();
 }
--- project/application/Stages/Stage/MirrorStage.h
+++ project/application/Stages/Stage/MirrorStage.h
@@ -1,11 +1,96 @@
 #pragma once
 
 #include "Stages/BaseStage.h"
+#include"GameObject/TestField/TestField.h"
+
+#include"GameObject/Portal/PortalManager.h"
+#include"GameObject/WhiteBoard/WhiteBoardManager.h"
+#include"GameObject/TimeCard/TimeCardWatch.h"
+#include"GameObject/TimeCard/TimeCard.h"
+#include"GameObject/TimeCard/TimeCardRack.h"
+#include"GameObject/Flashlight/Flashlight.h"
+#include"GameObject/Key/Key.h"
+#include <GameObject/Chair/ChairManager.h>
+#include"GameObject/Wall/WallManager.h"
+#include"GameObject/Wall/WallManager2.h"
+
+#include"GameObject/VendingMac/VendingMac.h"
+#include"GameObject/Door/Door.h"
+#include"GameObject/Locker/LockerManager.h"
+#include"GameObject/Desk/DeskManager.h"
+#include"GameObject/Box/BoxManager.h"
+#include "Menu/Menu.h"
+
+#include <GameObject/Edamame/Edamame.h>
+#include"GameObject/PC/PC.h"
+
+#include "GameObject/Coffee/Coffees.h"
+
+
+
+#pragma region // Event
+#include "GameObject/Event/FirstGameEvent.h"
+#pragma endregion
+#include"GameObject/YoshidaMath/CollisionManager/CollisionManager.h"
 
 class MirrorStage : public BaseStage {
+
+	   // TestField
+	std::unique_ptr<TestField> testField_ = nullptr;
+	// ホワイトボード管理
+	std::unique_ptr<WhiteBoardManager> whiteBoardManager_ = nullptr;
+	// ポータル管理
+	std::unique_ptr<PortalManager> portalManager_ = nullptr;
+	// 携帯打刻機
+	std::unique_ptr<TimeCardWatch> timeCardWatch_ = nullptr;
+	// 懐中電灯
+	std::unique_ptr<Flashlight> flashlight_ = nullptr;
+	// 鍵
+	std::unique_ptr<Key> key_ = nullptr;
+	// 枝豆
+	std::unique_ptr<Edamame> edamame_ = nullptr;
+	// 椅子
+	std::unique_ptr<ChairManager> chairManager_ = nullptr;
+	// 壁管理
+	std::unique_ptr<WallManager> wallManager_ = nullptr;
+	// 壁管理2こめ
+	std::unique_ptr<WallManager2> wallManager2_ = nullptr;
+
+	    // 自販機
+	std::unique_ptr<VendingMac> vendingMac_ = nullptr;
+	// ドア
+	std::unique_ptr<Door> door_ = nullptr;
+	// ロッカー管理
+	std::unique_ptr<LockerManager> lockerManager_ = nullptr;
+	// デスク管理
+	std::unique_ptr<DeskManager> deskManager_ = nullptr;
+	// タイムカード
+	std::unique_ptr<TimeCard> timeCard_ = nullptr;
+	std::unique_ptr<TimeCardRack> timeCardRack_ = nullptr;
+	// 箱
+	std::unique_ptr<BoxManager> boxManager_ = nullptr;
+
+	   // PC
+	std::unique_ptr<PC> pc_ = nullptr;
+	// コーヒー缶
+	std::unique_ptr<Coffees> coffees_ = nullptr;
+
+	// 衝突管理
+	std::unique_ptr<CollisionManager> collisionManager_ = nullptr;
+	// 最初のイベント
+	std::unique_ptr<FirstGameEvent> firstEvent_ = nullptr;
+	GameEvent* currentEvent_ = nullptr;
+
 public:
+
+	MirrorStage();
 	void Initialize() override;
 	void Update() override;
 	void Draw() override;
 	void Finalize() override;
+
+	private:
+	void CheckCollision();
+
+
 };
