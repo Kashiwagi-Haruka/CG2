9362b5a44af65cf9612f66f1ce940377eb4c6edd
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-04-13 15:17:10 +0900

    a

--- project/Engine/Editor/EditorTool/Asset/EditorAsset.cpp
+++ project/Engine/Editor/EditorTool/Asset/EditorAsset.cpp
@@ -1,4 +1,5 @@
 #include "EditorAsset.h"
+#include "Engine/Texture/TextureManager.h"
 #include <algorithm>
 #include <imgui.h>
 
@@ -27,7 +28,34 @@ void EditorAsset::EditorDraw() {
 	ImGui::SetNextWindowPos(ImVec2(assetWindowPosX, assetWindowPosY), ImGuiCond_Always);
 	ImGui::SetNextWindowSize(ImVec2(assetWindowWidth, assetWindowHeight), ImGuiCond_Always);
 	if (ImGui::Begin("Asset", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_HorizontalScrollbar)) {
-		ImGui::TextUnformatted("Editor Asset Window");
+		constexpr const char* kFileIconPath = "Resources/Editor/File.png";
+		const ImVec2 buttonSize(96.0f, 112.0f);
+		const ImVec2 iconSize(48.0f, 48.0f);
+		const float iconTopOffset = 16.0f;
+		const float textBottomOffset = 14.0f;
+		const auto fileIconHandle = TextureManager::GetInstance()->GetSrvHandleGPU(kFileIconPath);
+
+		ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(0, 0, 0, 0));
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, IM_COL32(255, 255, 255, 32));
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, IM_COL32(255, 255, 255, 48));
+		ImGui::PushStyleColor(ImGuiCol_Border, IM_COL32(0, 0, 0, 0));
+		ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 0.0f);
+
+		const ImVec2 buttonPos = ImGui::GetCursorScreenPos();
+		if (ImGui::Button("##AudioAssetButton", buttonSize)) {
+		}
+
+		ImDrawList* drawList = ImGui::GetWindowDrawList();
+		const ImVec2 iconPos(buttonPos.x + (buttonSize.x - iconSize.x) * 0.5f, buttonPos.y + iconTopOffset);
+		drawList->AddImage(ImTextureRef(reinterpret_cast<void*>(fileIconHandle.ptr)), iconPos, ImVec2(iconPos.x + iconSize.x, iconPos.y + iconSize.y));
+
+		const char* label = "Audio";
+		const ImVec2 labelSize = ImGui::CalcTextSize(label);
+		const ImVec2 labelPos(buttonPos.x + (buttonSize.x - labelSize.x) * 0.5f, buttonPos.y + buttonSize.y - textBottomOffset - labelSize.y);
+		drawList->AddText(labelPos, IM_COL32(255, 255, 255, 255), label);
+
+		ImGui::PopStyleVar();
+		ImGui::PopStyleColor(4);
 	}
 	ImGui::End();
 }
