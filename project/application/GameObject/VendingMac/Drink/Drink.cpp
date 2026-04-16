f225af47aff95e829d5774c0ad5a1731206b664d
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-04-16 15:49:10 +0900

    a

--- project/application/GameObject/VendingMac/Drink/Drink.cpp
+++ project/application/GameObject/VendingMac/Drink/Drink.cpp
@@ -1,13 +1,16 @@
 #include "Drink.h"
-#include <Model/ModelManager.h>
-#include "Object3d/Object3dCommon.h"
+#include "Camera.h"
 #include "Function.h"
-namespace {
-const int kMaxDrinkCount = 10;
-const Vector3 kDrinkPositionOffset = {0.0f, 1.3f, 0.6f};
-}
-Drink::Drink() {
+#include "Object3d/Object3dCommon.h"
+#include <Model/ModelManager.h>
 
+namespace {
+constexpr int kMaxDrinkCount = 10;
+const Vector3 kDrinkPositionOffset = {0.0f, 0.9f, 0.45f};
+const Vector3 kDrinkScale = {1.0f, 1.0f, 1.0f};
+} // namespace
+
+Drink::Drink() {
 	ModelManager::GetInstance()->LoadModel("Resources/TD3_3102/3d/Drink/water", "water");
 	ModelManager::GetInstance()->LoadModel("Resources/TD3_3102/3d/Drink/tea", "tea");
 	ModelManager::GetInstance()->LoadModel("Resources/TD3_3102/3d/Drink/energyDrink", "energyDrink");
@@ -20,8 +23,8 @@ Drink::Drink() {
 	severedHeadObj_ = std::make_unique<Object3d>();
 	coffeeObj_ = std::make_unique<Object3d>();
 }
-void Drink::Initialize() {
 
+void Drink::Initialize() {
 	waterObj_->SetModel("water");
 	waterObj_->Initialize();
 	teaObj_->SetModel("tea");
@@ -33,60 +36,57 @@ void Drink::Initialize() {
 	coffeeObj_->SetModel("coffee");
 	coffeeObj_->Initialize();
 }
+
 bool Drink::ChangeDrink() {
-	drinkCount_++;	
+	++drinkCount_;
 	if (drinkCount_ >= kMaxDrinkCount) {
+		drinkCount_ = kMaxDrinkCount;
 		return true;
 	}
 	currentDrinkName_ = static_cast<DrinkName>((static_cast<int>(currentDrinkName_) + 1) % kMaxDrinkNameCount);
 	return false;
 }
-void Drink::Update() {
-	
+
+Object3d* Drink::GetCurrentDrinkObject() {
 	switch (currentDrinkName_) {
 	case Drink::WATER:
-		waterObj_->Update();
-		break;
+		return waterObj_.get();
 	case Drink::TEA:
-		teaObj_->Update();
-		break;
+		return teaObj_.get();
 	case Drink::COFFEE:
-		coffeeObj_->Update();
-		break;
+		return coffeeObj_.get();
 	case Drink::ENERGY:
-		energyDrinkObj_->Update();
-		break;
+		return energyDrinkObj_.get();
 	case Drink::SeveredHead:
-		severedHeadObj_->Update();
-		break;
-	case Drink::kMaxDrinkNameCount:
-		break;
+		return severedHeadObj_.get();
 	default:
-		break;	
+		return nullptr;
 	}
 }
-void Drink::Draw() {
-	Object3dCommon::GetInstance()->DrawCommon();
-	switch (currentDrinkName_) {
-	case Drink::WATER:
-		waterObj_->Draw();
-		break;
-	case Drink::TEA:
-		teaObj_->Draw();
-		break;
-	case Drink::COFFEE:
-		coffeeObj_->Draw();
-		break;
-	case Drink::ENERGY:
-		energyDrinkObj_->Draw();
-		break;
-	case Drink::SeveredHead:
-		severedHeadObj_->Draw();
-		break;
-	case Drink::kMaxDrinkNameCount:
-		break;
-	default:
-		break;
+
+void Drink::Update() {
+	Object3d* currentDrink = GetCurrentDrinkObject();
+	if (!currentDrink) {
+		return;
 	}
 
+	currentDrink->SetTranslate(vendingMacPosition_ + kDrinkPositionOffset);
+	currentDrink->SetScale(kDrinkScale);
+	currentDrink->Update();
+}
+
+void Drink::Draw() {
+	Object3dCommon::GetInstance()->DrawCommon();
+	Object3d* currentDrink = GetCurrentDrinkObject();
+	if (currentDrink) {
+		currentDrink->Draw();
+	}
+}
+
+void Drink::SetCamera(Camera* camera) {
+	waterObj_->SetCamera(camera);
+	teaObj_->SetCamera(camera);
+	coffeeObj_->SetCamera(camera);
+	energyDrinkObj_->SetCamera(camera);
+	severedHeadObj_->SetCamera(camera);
 }
--- project/application/GameObject/VendingMac/VendingMac.cpp
+++ project/application/GameObject/VendingMac/VendingMac.cpp
@@ -1,114 +1,104 @@
 #include "VendingMac.h"
-#include"Model/ModelManager.h"
-#include"Function.h"
-#include"GameObject/YoshidaMath/YoshidaMath.h"
-#include"GameObject/KeyBindConfig.h"
-#include<imgui.h>
-#include"GameObject/SEManager/SEManager.h"
-#include"Object3d/Object3dCommon.h"
+#include "Function.h"
+#include "GameObject/KeyBindConfig.h"
+#include "GameObject/SEManager/SEManager.h"
+#include "GameObject/YoshidaMath/YoshidaMath.h"
+#include "Model/ModelManager.h"
+#include "Object3d/Object3dCommon.h"
+#include <imgui.h>
 
 bool VendingMac::isRayHit_ = false;
 namespace {
 const Vector4 kRayHitOutlineColor = {1.0f, 1.0f, 0.0f, 1.0f};
 const float kRayHitOutlineWidth = 26.0f;
 } // namespace
-VendingMac::VendingMac()
-{
+VendingMac::VendingMac() {
 
-    obj_ = std::make_unique<Object3d>();
-    ModelManager::GetInstance()->LoadModel("Resources/TD3_3102/3d/vendingMac", "vendingMac");
-    obj_->SetModel("vendingMac");
-    SetAABB({ .min = {-0.75f,0.0f,-0.75f},.max = {0.75f,1.83f,0.75f} });
-    SetCollisionAttribute(kCollisionWall);
-    SetCollisionMask(kCollisionPlayer);
+	obj_ = std::make_unique<Object3d>();
+	drink_ = std::make_unique<Drink>();
+	ModelManager::GetInstance()->LoadModel("Resources/TD3_3102/3d/vendingMac", "vendingMac");
+	obj_->SetModel("vendingMac");
+	SetAABB({
+	    .min = {-0.75f, 0.0f,  -0.75f},
+          .max = {0.75f,  1.83f, 0.75f }
+    });
+	SetCollisionAttribute(kCollisionWall);
+	SetCollisionMask(kCollisionPlayer);
 
-    areaLight_.color = { 1.0f,234.0f / 255.0f,200.0f / 255.0f,1.0f };
-    areaLight_.intensity = 3.1f;
-    areaLight_.width = 2.0f;
-    areaLight_.height = 0.8f;
-    areaLight_.radius = 0.8f;
-    areaLight_.decay = 1.0f;
-    translate_ = { 0.0f,1.3f,0.6f };
+	areaLight_.color = {1.0f, 234.0f / 255.0f, 200.0f / 255.0f, 1.0f};
+	areaLight_.intensity = 3.1f;
+	areaLight_.width = 2.0f;
+	areaLight_.height = 0.8f;
+	areaLight_.radius = 0.8f;
+	areaLight_.decay = 1.0f;
+	translate_ = {0.0f, 1.3f, 0.6f};
 }
 
-VendingMac::~VendingMac()
-{
-    SEManager::StopSound(SEManager::NOISE);
+VendingMac::~VendingMac() { SEManager::StopSound(SEManager::NOISE); }
+
+void VendingMac::OnCollision(Collider* collider) {
+	if (collider->GetCollisionAttribute() == kCollisionPlayer) {
+	}
 }
 
-void VendingMac::OnCollision(Collider* collider)
-{
-    if (collider->GetCollisionAttribute() == kCollisionPlayer) {
+Vector3 VendingMac::GetWorldPosition() const { return obj_->GetTranslate(); }
 
-    }
+void VendingMac::Update() {
+	CheckCollision();
+
+	obj_->Update();
+	drink_->SetVendingMacPosition(obj_->GetTranslate());
+	drink_->Update();
+
+	Matrix4x4 worldMat = Function::Multiply(Function::MakeTranslateMatrix(translate_), obj_->GetWorldMatrix());
+	areaLight_.position = Function::TransformVM({0.0f, 0.0f, 0.0f}, worldMat);
+	areaLight_.normal = -1.0f * YoshidaMath::GetForward(obj_->GetWorldMatrix());
+
+	// プレイヤーのカメラ位置から
+	Vector3 distance = playerCamera_->GetRay().origin - obj_->GetTranslate();
+	float length = Function::Length(distance);
+	SEManager::SetVol(GetVol(length, 1.0f), SEManager::NOISE);
 }
 
-Vector3 VendingMac::GetWorldPosition() const
-{
-    return obj_->GetTranslate();
-}
-
-void VendingMac::Update()
-{
-    CheckCollision();
-
-    obj_->Update();
-
-    Matrix4x4 worldMat = Function::Multiply(Function::MakeTranslateMatrix(translate_), obj_->GetWorldMatrix());
-    areaLight_.position = Function::TransformVM({ 0.0f,0.0f,0.0f }, worldMat);
-    areaLight_.normal = -1.0f * YoshidaMath::GetForward(obj_->GetWorldMatrix());
-
-    // プレイヤーのカメラ位置から
-    Vector3 distance = playerCamera_->GetRay().origin - obj_->GetTranslate();
-    float  length = Function::Length(distance);
-    SEManager::SetVol(GetVol(length, 1.0f), SEManager::NOISE);
-
-
-
-}
-
-void VendingMac::Initialize()
-{
-    isRayHit_ = false;
-  	interactRequested_ = false;
-    obj_->Initialize();
-    SEManager::SoundPlay(SEManager::NOISE, true);
+void VendingMac::Initialize() {
+	isRayHit_ = false;
+	interactRequested_ = false;
+	obj_->Initialize();
+	drink_->Initialize();
+	SEManager::SoundPlay(SEManager::NOISE, true);
 	obj_->SetOutlineColor(kRayHitOutlineColor);
 	obj_->SetOutlineWidth(kRayHitOutlineWidth);
 }
 
-void VendingMac::Draw() { 
-    if (isRayHit_) {
+void VendingMac::Draw() {
+	if (isRayHit_) {
 		Object3dCommon::GetInstance()->DrawCommonOutline();
 	} else {
 		Object3dCommon::GetInstance()->DrawCommon();
-    }
-    obj_->Draw(); 
+	}
+	obj_->Draw();
+	drink_->Draw();
 }
 
-
-void VendingMac::CheckCollision()
-{
-    isRayHit_ = OnCollisionRay();
+void VendingMac::CheckCollision() {
+	isRayHit_ = OnCollisionRay();
 
 	if (isRayHit_) {
 		if (PlayerCommand::GetInstance()->InteractTrigger()) {
-			
-                 if (SEManager::IsSoundFinished(SEManager::VENDING_MAC)) {
-                SEManager::SoundPlay(SEManager::VENDING_MAC);
-            }
-            
-            
-            if (rand() % 4 == 0) {
-           
-                if (!interactRequested_) {
-                    interactRequested_ = true;
-                }
-            }
-    
+
+			if (SEManager::IsSoundFinished(SEManager::VENDING_MAC)) {
+				SEManager::SoundPlay(SEManager::VENDING_MAC);
+			}
+
+			if (!interactRequested_) {
+				interactRequested_ = true;
+			}
+
+			if (drink_->ChangeDrink()) {
+				interactRequested_ = true;
+			}
 		}
 	}
-
 }
 
 float VendingMac::GetVol(float length, float maxVol) {
@@ -128,6 +118,7 @@ void VendingMac::SetPlayerCamera(PlayerCamera* camera) { playerCamera_ = camera;
 
 void VendingMac::SetCamera(Camera* camera) {
 	obj_->SetCamera(camera);
+	drink_->SetCamera(camera);
 	obj_->UpdateCameraMatrices();
 }
 Vector3 VendingMac::GetForward() const { return YoshidaMath::GetForward(obj_->GetWorldMatrix()); }
