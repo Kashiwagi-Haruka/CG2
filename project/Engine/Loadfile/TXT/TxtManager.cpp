773f898e3231419359c877f74678da369a3c97c1
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-05-07 16:41:10 +0900

    クレジット

--- project/Engine/Loadfile/TXT/TxtManager.cpp
+++ project/Engine/Loadfile/TXT/TxtManager.cpp
@@ -1,1 +1,21 @@
 #include "TxtManager.h"
+
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+
+TxtManager* TxtManager::GetInstance() {
+	static TxtManager instance;
+	return &instance;
+}
+
+std::string TxtManager::LoadTxtAsString(const std::string& filePath) const {
+	std::ifstream file(filePath);
+	if (!file.is_open()) {
+		throw std::runtime_error("TxtManager: Failed to open file: " + filePath);
+	}
+
+	std::ostringstream buffer;
+	buffer << file.rdbuf();
+	return buffer.str();
+}
--- project/Engine/Loadfile/TXT/TxtManager.h
+++ project/Engine/Loadfile/TXT/TxtManager.h
@@ -1,2 +1,14 @@
 #pragma once
-class TxtManager {};
+
+#include <string>
+
+class TxtManager {
+public:
+	static TxtManager* GetInstance();
+
+	// 指定したtxtファイルを読み込み、内容を1つの文字列として返す
+	std::string LoadTxtAsString(const std::string& filePath) const;
+
+private:
+	TxtManager() = default;
+};
