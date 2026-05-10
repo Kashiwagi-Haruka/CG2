1083bc7850aafac6568c1980d1f1aee312450328
めいそ <k024g0101@g.neec.ac.jp>
2026-05-11 01:07:12 +0900

    トイレ部屋の追加

---
+++ project/application/GameObject/Toilet/Toilet.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <Transform.h>
+#include <string>
+#include <Camera.h>
+#include <RigidBody.h>
+#include <memory>
+#include <Object3d/Object3d.h>
+#include <GameObject/GameCamera/PlayerCamera/PlayerCamera.h>
+#include <GameObject/YoshidaMath/CollisionManager/Collider.h>
+#include "Animation/Animation.h"
+#include "Animation/Skeleton.h"
+#include "Animation/SkinCluster.h"
+
+class Toilet : public YoshidaMath::Collider {
+public:
+    Toilet();
+    void Initialize();
+    void Update();
+    void Draw();
+    static void SetPlayerCamera(PlayerCamera* camera) { playerCamera_ = camera; };
+    void SetEditorRegistrationName(const std::string& name) { editorRegistrationName_ = name; }
+    void SetCamera(Camera* camera);
+    void CheckCollision();
+    bool OnCollisionRay();
+    /// @brief 衝突時コールバック関数
+    void OnCollision(Collider* collider)override;
+    /// @brief ワールド座標を取得する
+    /// @return ワールド座標
+    Vector3 GetWorldPosition() const override;
+    bool IsRayHit() { return isRayHit_; }
+    const Matrix4x4& GetWorldMatrix() const { return obj_->GetWorldMatrix(); };
+    void SetAnimationGroupName(const std::string& name) { animationGroupName_ = name; }
+    bool GetIsOpen() { return isOpen_; }
+private:
+    void Animation();
+private:
+    bool isOpen_ = false;
+    bool isRayHit_ = false;
+    static PlayerCamera* playerCamera_;
+    std::unique_ptr<Object3d>obj_ = nullptr;
+
+    // アニメーション
+    Animation::AnimationData blendedPoseAnimation_{};
+    // 骨
+    std::unique_ptr<Skeleton> skeleton_{};
+    // スキン
+    SkinCluster skinCluster_{};
+    std::string animationGroupName_ = "Toilet";
+    const float kAnimationBlendDuration_ = 0.5f;
+    bool animationFinished_ = false;
+    std::string desiredAnimationName = "Close";
+    std::string editorRegistrationName_ = "Toilet";
+};
