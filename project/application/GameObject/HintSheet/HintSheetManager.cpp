1f87d551b66f68e4d896fcee0c2de08ee9e810b7
めいそ <k024g0101@g.neec.ac.jp>
2026-05-11 16:44:19 +0900

    ヒント

---
+++ project/application/GameObject/HintSheet/HintSheetManager.cpp
@@ -0,0 +1,72 @@
+#include "HintSheetManager.h"
+#include"ScreenSize.h"
+#include"TextureManager.h"
+#include"SpriteCommon.h"
+
+void HintSheetManager::Initialize() {
+    fontHandle_ = FreeTypeManager::CreateFace("Resources/TD3_3102/Irohakaku/irohakakuC-Medium.ttf", 0);
+    FreeTypeManager::SetPixelSizes(fontHandle_, 32, 32);
+    hintSheets_.clear();
+    uint32_t handle = TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/white2x2.png");
+    sprite_ = std::make_unique<Sprite>();
+    sprite_->Initialize(handle);
+    sprite_->SetAnchorPoint({ 0.5f,0.5f });
+    sprite_->SetRotation(0.0f);
+    sprite_->SetScale({ SCREEN_SIZE::WIDTH,SCREEN_SIZE::HEIGHT });
+    sprite_->SetPosition({ SCREEN_SIZE::HALF_WIDTH,SCREEN_SIZE::HALF_HEIGHT });
+    sprite_->SetColor({ 0.0f,0.0f,0.0f,0.5f });
+    sprite_->Update();
+}
+
+void HintSheetManager::AddHintSheet(const std::string& textFilePath) {
+    auto newSheet = std::make_unique<HintSheet>();
+
+    // 初期化（フォントとファイルパスを渡す）
+    newSheet->Initialize(fontHandle_, textFilePath);
+
+    // すでにカメラが設定されている場合は適用する
+    if (camera_) {
+        newSheet->SetCamera(camera_);
+    }
+
+    hintSheets_.push_back(std::move(newSheet));
+}
+
+void HintSheetManager::Update() {
+    for (auto& sheet : hintSheets_) {
+        sheet->Update();
+    }
+}
+
+void HintSheetManager::Draw() {
+    for (auto& sheet : hintSheets_) {
+        sheet->Draw();
+    }
+}
+
+void HintSheetManager::DrawUI() {
+
+    for (auto& sheet : hintSheets_) {
+        if (sheet->GetIsLooking()) {
+            SpriteCommon::GetInstance()->DrawCommon();
+            sprite_->Draw();
+            break;
+        }
+    }
+
+    for (auto& sheet : hintSheets_) {
+        // 現在「見ている」状態のシートだけが内部でDrawを実行します
+        sheet->DrawUI();
+    }
+}
+
+void HintSheetManager::SetCamera(Camera* camera) {
+    camera_ = camera;
+    for (auto& sheet : hintSheets_) {
+        sheet->SetCamera(camera);
+    }
+}
+
+void HintSheetManager::SetPlayerCamera(PlayerCamera* playerCamera) {
+    HintSheet::SetPlayerCamera(playerCamera);
+}
---
+++ project/application/GameObject/HintSheet/HintSheetManager.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include "HintSheet.h"
+#include "Sprite.h"
+
+class HintSheetManager {
+public:
+    HintSheetManager() = default;
+    ~HintSheetManager() = default;
+
+    /// @brief 初期化
+    void Initialize();
+
+    /// @brief ヒントシートを追加する
+    /// @param transform 配置場所
+    /// @param textFilePath 読み込むテキストファイルパス
+    void AddHintSheet( const std::string& textFilePath);
+
+    void Update();
+
+    /// @brief 3Dモデルの描画
+    void Draw();
+
+    /// @brief テキストUIの描画
+    void DrawUI();
+
+    /// @brief 全ヒントシートにカメラをセット
+    void SetCamera(Camera* camera);
+
+    /// @brief プレイヤーカメラをセット
+    void SetPlayerCamera(PlayerCamera* playerCamera);
+
+    /// @brief 全ヒントシートのリストを取得
+    std::vector<std::unique_ptr<HintSheet>>& GetHintSheets() { return hintSheets_; }
+
+private:
+    std::unique_ptr<Sprite>sprite_ = nullptr;
+    std::vector<std::unique_ptr<HintSheet>> hintSheets_;
+    uint32_t fontHandle_ = 0;
+    Camera* camera_ = nullptr;
+};
