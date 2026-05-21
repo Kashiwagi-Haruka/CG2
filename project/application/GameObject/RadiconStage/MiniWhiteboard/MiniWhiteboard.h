dace68994f1fd560690f126150d9006526c0edd8
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-05-21 16:51:23 +0900

    a

--- project/application/GameObject/RadiconStage/MiniWhiteboard/MiniWhiteboard.h
+++ project/application/GameObject/RadiconStage/MiniWhiteboard/MiniWhiteboard.h
@@ -3,6 +3,7 @@
 #include "RigidBody.h"
 #include "Transform.h"
 #include "Vector3.h"
+#include "Object3d/Object3d.h"
 
 class MiniWhiteboard {
 public:
@@ -18,6 +19,7 @@ private:
 	bool IsPlayerTouchingPortal(const Vector3& playerPosition, float playerRadius) const;
 
 private:
+	std::unique_ptr<Object3d> obj_;
 	Transform transform_{};
 	Vector3 portalLocalOffset_{0.0f, 0.9f, 0.0f};
 	Vector3 portalHalfSize_{0.22f, 0.35f, 0.08f};
