395016e3c1579dcb563f92e0e2bdc982224e41ce
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-05-25 11:04:23 +0900

    a

--- project/application/MiniMap/MiniMap.h
+++ project/application/MiniMap/MiniMap.h
@@ -2,11 +2,12 @@
 #include <string>
 #include "Vector3.h"
 #include "Vector4.h"
+#include <memory>
 class Object3d;
 class Camera;
 class MiniMap {
 	
-	Camera* camera;
+	std::unique_ptr<Camera> camera;
 
 public:
 	void AddObject(std::string Name, Object3d* object,Vector4 color);
